4159f5a017af5775fba607248ed75135f78f4a1f
Ibrahim Al Busaidi <i.s.busaidi@gmail.com>
2024-11-12 16:57:32 +0400

    Adding ERD and Mapping and files for initial implementation

---
+++ Implementation/C#/OOP_EFCore_DB_Project_Implementation/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace OOP_EFCore_DB_Project_Implementation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
