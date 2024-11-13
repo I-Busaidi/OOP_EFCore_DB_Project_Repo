3a034d74253813f7e5b9cb2cc9fc0aa6efd7317b
Ibrahim Al Busaidi <i.s.busaidi@gmail.com>
2024-11-13 10:17:18 +0400

    added methods to the user repository implementing IUsersRepo interface.

--- Implementation/C#/OOP_EFCore_DB_Project_Implementation/Repositories/UserRepo.cs
+++ Implementation/C#/OOP_EFCore_DB_Project_Implementation/Repositories/UserRepo.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using OOP_EFCore_DB_Project_Implementation.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,53 @@ using System.Threading.Tasks;
 
 namespace OOP_EFCore_DB_Project_Implementation.Repositories
 {
-    internal class UserRepo
+    public class UserRepo : IUsersRepo<User>
     {
+        private readonly LibraryAppDbContext _context;
+        public UserRepo(LibraryAppDbContext context)
+        {
+            _context = context;
+        }
+
+        public IEnumerable<User> GetAll()
+        {
+            return _context.Users.ToList();
+        }
+
+        public User GetByName(string fname, string lname)
+        {
+            return _context.Users.FirstOrDefault(u => u.FName == fname && u.LName == lname);
+        }
+
+        public void Insert(User user)
+        {
+            _context.Users.Add(user);
+            _context.SaveChanges();
+        }
+
+        public void UpdateByName(User user, string fname, string lname)
+        {
+            var userToUpdate = GetByName(fname, lname);
+            if (userToUpdate != null)
+            {
+                _context.Entry(userToUpdate).CurrentValues.SetValues(user);
+                _context.SaveChanges();
+            }
+        }
+
+        public void DeleteById(int uId)
+        {
+            var user = _context.Users.Find(uId);
+            if (user != null)
+            {
+                _context.Users.Remove(user);
+                _context.SaveChanges();
+            }
+        }
+
+        public int CountByGender(string gender)
+        {
+            return _context.Users.Count(u => u.Gender == gender);
+        }
     }
 }
