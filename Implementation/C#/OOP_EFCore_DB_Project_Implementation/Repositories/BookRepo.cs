21535a78717049138e0c27e9bba7ffe65fa866d2
Ibrahim Al Busaidi <i.s.busaidi@gmail.com>
2024-11-13 10:46:45 +0400

    added methods to the book repository.

--- Implementation/C#/OOP_EFCore_DB_Project_Implementation/Repositories/BookRepo.cs
+++ Implementation/C#/OOP_EFCore_DB_Project_Implementation/Repositories/BookRepo.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using OOP_EFCore_DB_Project_Implementation.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,60 @@ using System.Threading.Tasks;
 
 namespace OOP_EFCore_DB_Project_Implementation.Repositories
 {
-    internal class BookRepo
+    public class BookRepo : IObjectsRepo<Book>
     {
+        private readonly LibraryAppDbContext _context;
+        public BookRepo(LibraryAppDbContext context)
+        {
+            _context = context;
+        }
+
+        public IEnumerable<Book> GetAll()
+        {
+            return _context.Books.ToList();
+        }
+        public Book GetByName(string name)
+        {
+            return _context.Books.FirstOrDefault(book => book.BookName == name);
+        }
+        public void Insert(Book book)
+        {
+            _context.Books.Add(book);
+            _context.SaveChanges();
+        }
+        public void UpdateByName(Book book, string name)
+        {
+            var bookToUpdate = GetByName(name);
+            if (bookToUpdate != null)
+            {
+                _context.Entry(bookToUpdate).CurrentValues.SetValues(book);
+                _context.SaveChanges();
+            }
+        }
+        public void DeleteById(int id)
+        {
+            var book = _context.Books.Find(id);
+            if (book != null)
+            {
+                _context.Books.Remove(book);
+                _context.SaveChanges();
+            }
+        }
+        public decimal GetTotalPrice()
+        {
+            return _context.Books.Sum(book => book.CopyPrice);
+        }
+        public decimal GetMaxPrice()
+        {
+            return _context.Books.Max(book => book.CopyPrice);
+        }
+        public int GetTotalBorrowedBooks()
+        {
+            return _context.Borrows.Count(b => b.IsReturned == false);
+        }
+        public int GetTotalBooksPerCategoryName(string name)
+        {
+            return _context.Books.Count(book => book.Category.CatName == name);
+        }
     }
 }
